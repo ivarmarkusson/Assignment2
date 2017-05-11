ff935ae0862df703cadabff112ae5f88d62b686f
ivarmarkusson <ivarmar15@ru.is>
2017-05-11 18:28:33 +0000

    == operator for pieces

--- BoardGameAssignment/Piece.h
+++ BoardGameAssignment/Piece.h
@@ -15,11 +15,11 @@ class Piece
         Piece();
         Piece(int player,char sym,Position pos, std::vector<std::pair<int,int>> moves);
         virtual ~Piece();
-        char get_symbol() {return symbol_;}
-        int get_owner() {return owner_;}
-        Position get_position() {return position_;}
+        char get_symbol() const {return symbol_;}
+        int get_owner() const {return owner_;}
+        Position get_position() const {return position_;}
         void set_position(int x, int y);
-        std::vector<std::pair<int,int>> get_posible_moves() {return posible_moves_;}
+        std::vector<std::pair<int,int>> get_posible_moves() const {return posible_moves_;}
         bool operator== (const Piece& rhs ) const;
     protected:
     private:
