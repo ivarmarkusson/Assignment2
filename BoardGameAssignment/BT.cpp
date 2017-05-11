6f5aec7b3d662e4bfb586902ad7569df1a4855e1
ivarmarkusson <ivarmar15@ru.is>
2017-05-11 14:05:26 +0000

    Pieces posible moves

--- BoardGameAssignment/BT.cpp
+++ BoardGameAssignment/BT.cpp
@@ -1,8 +1,22 @@
 #include "BT.h"
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 BT::BT(int rows, int cols) : Game(rows, cols)
 {
+    vector<pair<int,int>> movesPlayer1;
+    movesPlayer1.push_back(make_pair(1,1));
+    movesPlayer1.push_back(make_pair(0,1));
+    movesPlayer1.push_back(make_pair(-1,1));
+
+    vector<pair<int,int>> movesPlayer2;
+    movesPlayer2.push_back(make_pair(1,-1));
+    movesPlayer2.push_back(make_pair(0,-1));
+    movesPlayer2.push_back(make_pair(-1,-1));
+
+
     Position pos1, pos2;
     for (int i = 0; i < board_->get_columns(); i++)
     {
@@ -12,8 +26,8 @@ BT::BT(int rows, int cols) : Game(rows, cols)
             pos2.x_ = i;
             pos1.y_ = board_->get_rows()-j-1;
             pos2.y_ = j;
-            Piece p1(0,'P',pos1);
-            Piece p2(1,'p',pos2);
+            Piece p1(0,'P',pos1, movesPlayer1);
+            Piece p2(1,'p',pos2, movesPlayer2);
             board_->place_piece(p1);
             board_->place_piece(p2);
         }
--- BoardGameAssignment/Piece.h
+++ BoardGameAssignment/Piece.h
@@ -1,6 +1,8 @@
 #ifndef PIECE_H
 #define PIECE_H
 
+#include <vector>
+
 struct Position
 {
     int x_;
@@ -15,27 +17,30 @@ class Piece
             symbol_ = '.';
             owner_ = -1;
         };
-        Piece(int player,char sym,Position pos)
+        Piece(int player,char sym,Position pos, std::vector<std::pair<int,int>> moves)
         {
+            posible_moves_ = moves;
             symbol_ = sym;
             owner_ = player;
             position_.x_ = pos.x_;
             position_.y_ = pos.y_;
         }
-        virtual ~Piece() {};
-        char get_symbol() {return symbol_;};
-        int get_owner() {return owner_;};
-        Position get_position() {return position_;};
+        virtual ~Piece() {}
+        char get_symbol() {return symbol_;}
+        int get_owner() {return owner_;}
+        Position get_position() {return position_;}
         void set_position(int x, int y)
         {
             position_.x_ = x;
             position_.y_ = y;
-        };
+        }
+        std::vector<std::pair<int,int>> get_posible_moves() {return posible_moves_;}
     protected:
     private:
         char symbol_;
         int owner_;
         Position position_;
+        std::vector<std::pair<int,int>> posible_moves_;
 };
 
 #endif // PIECE_H
