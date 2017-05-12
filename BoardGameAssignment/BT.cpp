6999885930d287291e3b43b614bcf271ce53951c
Gylfian <gylfia13@ru.is>
2017-05-12 15:09:42 +0000

    Legal moves for BT

--- BoardGameAssignment/BT.cpp
+++ BoardGameAssignment/BT.cpp
@@ -1,6 +1,7 @@
 #include "BT.h"
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -39,8 +40,41 @@ BT::~BT()
 }
 
 
-vector<pair<Piece,Piece>> BT::legal_moves(){
-    vector<pair<Piece,Piece>> moves;
+vector<pair<Piece,Piece>> BT::legal_moves()
+{
+    int player_turn = turn_ % 2;
+    int enemy_turn = abs(turn_%2 - 1);
+    Position from;
+    Piece current_piece;
+    std::vector<std::pair<Piece,Piece>> moves;
+
+    for(int i = 0; i < board_->get_rows(); i++)
+    {
+        for(int j = 0; j < board_->get_columns(); j++)
+        {
+            from.x_ = i;
+            from.y_ = j;
+            current_piece = board_->get_at(from.x_,from.y_);
+            if(current_piece.get_owner() == player_turn)
+            {
+                std::vector<std::pair<int,int>> posible_moves = current_piece.get_posible_moves();
+                for(size_t k = 0; k < posible_moves.size(); k++)
+                {
+                    std::pair<int,int> move = posible_moves[k];
+                    int col = from.x_ + move.first;
+                    int row = from.y_ + move.second;
+                    if(col < board_->get_rows() && col >= 0 && row < board_->get_columns() && col >= 0)
+                    {
+                        Piece target = board_->get_at(col,row);
+                        if(target.get_owner() == -1 || (target.get_owner() == enemy_turn && abs(move.first) == 1))
+                        {
+                            moves.push_back(std::make_pair(current_piece, target));
+                        }
+                    }
+                }
+            }
+        }
+    }
     return moves;
 }
 
